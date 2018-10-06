31cde583814146f05dcefa17cb095964b7a5d79b
Gleb Karpushkin <rugleb@gmail.com>
2018-10-06 21:18:05 +0300

    redefine TypesNotEqual error

--- jsoncompare/errors.py
+++ jsoncompare/errors.py
@@ -1,5 +1,7 @@
 from abc import ABC
 
+from jsoncompare.utils import cls_name
+
 
 class Error(ABC):
     expected = None
@@ -26,6 +28,11 @@ class Error(ABC):
 
 class TypesNotEqual(Error):
     template = 'Types not equal. Expected: <{e}>, received: <{r}>'
+
+    def __init__(self, expected, actual):
+        e = cls_name(expected)
+        a = cls_name(actual)
+        super().__init__(e, a)
 
 
 class ValuesNotEqual(Error):
