403dace0e4c1f871305858634b25ad64690cd2f3
Gleb Karpushkin <rugleb@gmail.com>
2018-10-06 20:02:16 +0300

    moved Compare class to another file

--- jsoncompare/__init__.py
+++ jsoncompare/__init__.py
@@ -1,37 +1,5 @@
-import os
-import json
-import copy
-
-from .utils import *
+from jsoncompare.compare import Compare
 
 
-class Compare:
-    config = {}
-
-    def __init__(self, config=None):
-        self.set_config(config)
-
-    def set_config(self, config=None):
-        if config is None:
-            self.set_default_config()
-        self.config = config
-
-    def set_default_config(self):
-        directory = os.path.dirname(__file__)
-        path = '{}/{}'.format(directory, 'data/config.json')
-        with open(path, 'r') as fp:
-            config = json.load(fp)
-        self.set_config(config)
-
-    def check(self, expected, actual):
-        pass
-
-    def diff(self, expected, actual):
-        pass
-
-    def report(self, diff):
-        pass
-
-    @classmethod
-    def _prepare(cls, x):
-        return copy.deepcopy(x)
+if __name__ == '__main__':
+    c = Compare()
---
+++ jsoncompare/compare.py
@@ -0,0 +1,35 @@
+import os
+import json
+import copy
+
+
+class Compare:
+    config = {}
+
+    def __init__(self, config=None):
+        self.set_config(config)
+
+    def set_config(self, config=None):
+        if config is None:
+            self.set_default_config()
+        self.config = config
+
+    def set_default_config(self):
+        directory = os.path.dirname(__file__)
+        path = '{}/{}'.format(directory, 'data/config.json')
+        with open(path, 'r') as fp:
+            config = json.load(fp)
+        self.set_config(config)
+
+    def check(self, expected, actual):
+        pass
+
+    def diff(self, expected, actual):
+        pass
+
+    def report(self, diff):
+        pass
+
+    @classmethod
+    def _prepare(cls, x):
+        return copy.deepcopy(x)
