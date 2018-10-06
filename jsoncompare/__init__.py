e2a8b00aa95727ce848ad36bbd556ceb5562694e
Gleb Karpushkin <rugleb@gmail.com>
2018-10-06 19:00:58 +0300

    created methods for config initialization

--- jsoncompare/__init__.py
+++ jsoncompare/__init__.py
@@ -1,5 +1,20 @@
+import json
+
 from .utils import *
 
 
 class Compare:
-    pass
+    config = {}
+
+    def __init__(self, config=None):
+        self.set_config(config)
+
+    def set_config(self, config=None):
+        if config is None:
+            self.set_default_config()
+        self.config = config
+
+    def set_default_config(self):
+        with open('data/config.json', 'w') as fp:
+            config = json.load(fp)
+        self.set_config(config)
