192169e2e9b3b1b945c2280b310f5380adcd2382
Gleb Karpushkin <rugleb@gmail.com>
2018-10-07 18:55:48 +0300

    added test for config

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -9,3 +9,12 @@ class CompareTestCase(unittest.TestCase):
         c = Compare()
         self.assertIsInstance(c, Compare)
         self.assertIsInstance(c.config, dict)
+
+    def test_compare(self):
+        e = {'int': 1, 'str': 'Hello, world', 'float': 1.2345, 'bool': True}
+        a = {'int': 2, 'str': 'Hello, world', 'float': 1.23456, 'bool': False}
+        diff = Compare().check(e, a)
+        self.assertTrue('int' in diff)
+        self.assertTrue('bool' in diff)
+        self.assertTrue('str' not in diff)
+        self.assertTrue('float' not in diff)
