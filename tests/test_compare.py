792dbfe6bbeeafa613577005bca19eba64ace11a
Gleb Karpushkin <rugleb@gmail.com>
2018-10-08 10:11:23 +0300

    write tests

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -1,20 +1,68 @@
 import unittest
 
 from jsoncompare import Compare
+from jsoncompare.errors import ValuesNotEqual, TypesNotEqual, KeyNotExist, ValueNotFound, LengthNotEqual
 
 
 class CompareTestCase(unittest.TestCase):
 
-    def test_factory(self):
-        c = Compare()
-        self.assertIsInstance(c, Compare)
-        self.assertIsInstance(c.config.config, dict)
+    def test_compare_int(self):
+        diff = Compare().check(1, 1)
+        self.assertEqual(diff, {})
 
-    def test_compare(self):
-        e = {'int': 1, 'str': 'Hello, world', 'float': 1.2345, 'bool': True}
-        a = {'int': 2, 'str': 'Hello, world', 'float': 1.23456, 'bool': False}
+        diff = Compare().check(1, 2)
+        self.assertEqual(diff, ValuesNotEqual(1, 2).explain())
+
+    def test_compare_str(self):
+        diff = Compare().check('str', 'str')
+        self.assertEqual(diff, {})
+
+        diff = Compare().check('str1', 'str2')
+        self.assertEqual(diff, ValuesNotEqual('str1', 'str2').explain())
+
+    def test_compare_float(self):
+        diff = Compare().check(1.2, 1.2)
+        self.assertEqual(diff, {})
+
+        diff = Compare().check(1.23456, 1.23)
+        self.assertEqual(diff, {})
+
+        diff = Compare().check(1.2, 1.3)
+        self.assertEqual(diff, ValuesNotEqual)
+
+    def test_compare_bool(self):
+        diff = Compare().check(True, True)
+        self.assertEqual(diff, {})
+
+        diff = Compare().check(True, False)
+        self.assertEqual(diff, ValuesNotEqual(True, False).explain())
+
+    def test_compare_dict_diff(self):
+        e = {'int': 1, 'str': 'Hi', 'float': 1.23, 'bool': True}
+        a = {'int': 2, 'str': 'Hi', 'float': 1}
+
+        diff = Compare().check(e, e)
+        self.assertEqual(diff, {})
+
         diff = Compare().check(e, a)
-        self.assertTrue('int' in diff)
-        self.assertTrue('bool' in diff)
-        self.assertTrue('str' not in diff)
-        self.assertTrue('float' not in diff)
+        self.assertEqual(diff, {
+            'int': ValuesNotEqual(1, 2).explain(),
+            'float': TypesNotEqual(1.23, 1).explain(),
+            'bool': KeyNotExist('bool', None).explain()
+        })
+
+    def test_list_compare(self):
+        e = [1.23, 2, 'three', True]
+        a = [1.23, 3, 'three', False, None]
+
+        diff = Compare().check(e, e)
+        self.assertEqual(diff, {})
+
+        diff = Compare().check(e, a)
+        self.assertEqual(diff, {
+            '_length': LengthNotEqual(len(e), len(a)).explain(),
+            '_content': {
+                1: ValueNotFound(2, None).explain(),
+                3: ValueNotFound(True, None).explain()
+            }
+        })
