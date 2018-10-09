aee2194fa8fed120f2e4705cf19afafa4d948aed
Gleb Karpushkin <rugleb@gmail.com>
2018-10-10 00:53:14 +0300

    renamed project

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fd:
     long_description = fd.read()
 
 setuptools.setup(
-    name='jsoncompare',
+    name='comparejson',
     version='0.0.2',
     author='Gleb Karpushkin',
     author_email='rugleb@gmail.com',
