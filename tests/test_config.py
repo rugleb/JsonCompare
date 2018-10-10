820fa2e0d85b89e55dc7154bd7ff03af6ca4601f
Gleb Karpushkin <rugleb@gmail.com>
2018-10-10 18:23:24 +0300

    created test for Config get method

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,35 @@
 import unittest
 
+from jsoncompare.config import Config
+
+
+class ConfigTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.config = Config({
+            'language': {
+                'python': {
+                    'version': [
+                        3.5,
+                        3.6,
+                        3.7,
+                    ],
+                },
+                'php': 7.2,
+            },
+            'os': 'linux',
+        })
+
+    def test_get_method(self):
+        path = 'language.python.version'
+        self.assertIsInstance(self.config.get(path), list)
+
+        path = 'language.php'
+        self.assertEqual(self.config.get(path), 7.2)
+
+        path = 'os'
+        self.assertEqual(self.config.get(path), 'linux')
+
 
 if __name__ == '__main__':
     unittest.main()
