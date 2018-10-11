c183498f81e394c19c065fb0361b833fc3dfd7f8
Gleb Karpushkin <rugleb@gmail.com>
2018-10-11 21:07:11 +0300

    added setUp method

--- tests/test_ignore.py
+++ tests/test_ignore.py
@@ -1,7 +1,18 @@
+import os
+import json
 import unittest
 
 
 class IgnoreTestCase(unittest.TestCase):
+    obj = None
+    rules = None
+
+    def setUp(self):
+        curr_dir = os.path.dirname(__file__)
+        with open('{}/{}'.format(curr_dir, 'data/rules.json'), 'r') as fp:
+            self.rules = json.load(fp)
+        with open('{}/{}'.format(curr_dir, 'data/expected.json'), 'r') as fp:
+            self.obj = json.load(fp)
 
     def test_ignore(self):
         pass
