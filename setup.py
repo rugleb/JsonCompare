5d2820bf102d68c615d8371a81a5bebf33f0dd14
Gleb Karpushkin <rugleb@gmail.com>
2018-10-12 13:36:56 +0300

    changed package version to 0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fd:
 
 setuptools.setup(
     name='jsoncomparison',
-    version='0.0.4',
+    version='0.1.0',
     author='Gleb Karpushkin',
     author_email='rugleb@gmail.com',
     description='JSON comparator',
