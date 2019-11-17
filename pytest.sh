f4703585dd1a08c7fc18c7daf1efb6c534ac23db
Gleb Karpushkin <rugleb@gmail.com>
2019-11-17 21:55:37 +0300

    closed #7

--- pytest.sh
+++ pytest.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 coverage run --source jsoncomparison -m unittest
-coverage report -m -i
+coverage report -i -m
 coverage html -d coverage/html
+coverage xml -i -o coverage/index.xml
 coverage erase
