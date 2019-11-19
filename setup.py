d2c35f11aea92c13dcc7d0aefbf1e1577de53a5c
Gleb Karpushkin <rugleb@gmail.com>
2019-11-19 20:21:26 +0300

    formatting

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 import setuptools
 
-with open('README.md', 'r') as fd:
-    long_description = fd.read()
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setuptools.setup(
-    name='jsoncomparison',
-    version='0.1.1',
-    author='Gleb Karpushkin',
-    author_email='rugleb@gmail.com',
-    description='JSON comparator',
+    name="jsoncomparison",
+    version="0.1.1",
+    author="Gleb Karpushkin",
+    author_email="rugleb@gmail.com",
+    description="JSON comparator",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/rugleb/jsoncomparison',
+    long_description_content_type="text/markdown",
+    url="https://github.com/rugleb/jsoncomparison",
     packages=setuptools.find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
